084510c3d4cb737bb069370d573c6666986e4723
Yilin Ma <1135485226@qq.com>
2021-03-07 14:10:25 -0800

    Update ubuntu 20.04 setup script

--- ubuntu_setup_script.sh
+++ ubuntu_setup_script.sh
@@ -18,7 +18,7 @@ cd /root
 wget https://github.com/stefanberger/libtpms/archive/v0.7.7.tar.gz
 tar -zxvf v0.7.7.tar.gz
 cd libtpms-0.7.7/
-sudo apt-get -y install automake autoconf libtool gcc build-essential libssl-dev dh-exec pkg-config
+sudo apt-get -y install automake autoconf libtool gcc build-essential libssl-dev dh-exec pkg-config gawk
 ./autogen.sh --with-openssl
 make dist
 dpkg-buildpackage -us -uc -j4
@@ -31,8 +31,38 @@ wget https://github.com/stefanberger/swtpm/archive/v0.5.2.tar.gz
 tar -zxvf v0.5.2.tar.gz
 cd swtpm-0.5.2/
 apt-get -y install  libfuse-dev libglib2.0-dev libgmp-dev expect libtasn1-dev socat tpm-tools python3-twisted gnutls-dev gnutls-bin net-tools softhsm2 libseccomp-dev
+dpkg-buildpackage -us -uc -j$(nproc)
 cd ..
 dpkg -i swtpm-dev_0.5.2_amd64.deb swtpm-libs_0.5.2_amd64.deb swtpm-tools_0.5.2_amd64.deb swtpm_0.5.2_amd64.deb
 
 # install dbus
 apt-get install dbus
+
+# install tpm2-abrmd, tpm2-tools
+# apt-get install tpm2-abrmd tpm2-tools
+# just install tpm2-tools
+apt-get install tpm2-tools
+
+# install tpm2-tss dependencies
+apt-get -y install \
+  autoconf-archive \
+  libcmocka0 \
+  libcmocka-dev \
+  procps \
+  iproute2 \
+  build-essential \
+  git \
+  pkg-config \
+  gcc \
+  libtool \
+  automake \
+  libssl-dev \
+  uthash-dev \
+  autoconf \
+  doxygen \
+  libjson-c-dev \
+  libini-config-dev \
+  libcurl4-gnutls-dev
+# git clone https://github.com/tpm2-software/tpm2-tss.git
+# cd tpm2-tss/
+
