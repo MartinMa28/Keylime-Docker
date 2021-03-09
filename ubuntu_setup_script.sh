dbf1726efcf2f7970b26580236507879864a24cc
Yilin Ma <1135485226@qq.com>
2021-03-08 22:41:17 -0800

    Test ubuntu-20.04 script in MOC

--- ubuntu_setup_script.sh
+++ ubuntu_setup_script.sh
@@ -5,7 +5,7 @@ apt-get upgrade
 
 # install keylime v6.0.1
 apt-get install -y wget gcc make vim python3-pip
-apt-get install -y build-essential libssl-dev swig python3-dev libtss2-dev
+apt-get install -y build-essential libssl-dev swig python3-dev
 cd /root
 wget https://github.com/keylime/keylime/archive/6.0.1.tar.gz
 tar -zxvf 6.0.1.tar.gz
@@ -21,7 +21,7 @@ cd libtpms-0.7.7/
 apt-get -y install automake autoconf libtool gcc build-essential libssl-dev dh-exec pkg-config gawk
 ./autogen.sh --with-openssl
 make dist
-dpkg-buildpackage -us -uc -j4
+dpkg-buildpackage -us -uc -j$(nproc)
 cd ..
 dpkg -i libtpms0_0.7.7_amd64.deb libtpms-dev_0.7.7_amd64.deb
 
@@ -37,10 +37,6 @@ dpkg -i swtpm-dev_0.5.2_amd64.deb swtpm-libs_0.5.2_amd64.deb swtpm-tools_0.5.2_a
 
 # install dbus
 apt-get install dbus
-
-# install tpm2-abrmd, tpm2-tools
-# apt-get install tpm2-abrmd tpm2-tools
-
 
 # install tpm2-tss dependencies
 apt-get -y install \
@@ -64,6 +60,7 @@ apt-get -y install \
   libcurl4-gnutls-dev
 
 # install tpm2-tss
+cd /root
 wget https://github.com/tpm2-software/tpm2-tss/releases/download/2.4.5/tpm2-tss-2.4.5.tar.gz
 tar -zxvf tpm2-tss-2.4.5.tar.gz
 cd tpm2-tss-2.4.5
@@ -74,6 +71,7 @@ make install
 ldconfig
 
 # install tpm2-tools
+cd /root
 apt-get install -y uuid uuid-dev
 wget https://github.com/tpm2-software/tpm2-tools/releases/download/4.3.0/tpm2-tools-4.3.0.tar.gz
 tar -zxvf tpm2-tools-4.3.0.tar.gz
@@ -83,6 +81,7 @@ make -j$(nproc)
 make install
 
 # install tpm2-abrmd
+cd /root
 wget https://github.com/tpm2-software/tpm2-abrmd/releases/download/2.3.3/tpm2-abrmd-2.3.3.tar.gz
 tar -zxvf tpm2-abrmd-2.3.3.tar.gz
 cd tpm2-abrmd-2.3.3
@@ -93,4 +92,11 @@ apt-get install -y libglib2.0-dev
             --datarootdir=/usr/share
 make -j$(nproc)
 make install
+ldconfig
+pkill -HUP dbus-daemon
 
+# modify the tpm2-abrmd.service
+# ===============================
+# vim ...
+# ===============================
+systemctl daemon-reload
